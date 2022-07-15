cc91ffb56e6f8b34a554f099d35f417a86538a5c
Joe Ceresini <joe.ceresini@cleardata.com>
2022-07-15 16:13:18 -0400

    Add a separate EvaluationResult class for python policies to return

--- rpe/policy.py
+++ rpe/policy.py
@@ -14,10 +14,16 @@ class Policy:
 
 
 @dataclass
-class Evaluation:
+class _EvaluationTrigger:
     resource: Resource
     engine: Engine
     policy_id: str
+
+
+# To support python policies building their own evaluation, we need a class
+# that has the results of an eval without details about what triggered it
+@dataclass
+class EvaluationResult:
 
     compliant: bool
     remediable: bool
@@ -32,6 +38,9 @@ class Evaluation:
     #   * The prior `excluded` attribute can be implemented here
     evaluation_attributes: Optional[Dict[str, Any]] = field(default_factory=dict)
 
+
+@dataclass
+class Evaluation(EvaluationResult, _EvaluationTrigger):
     def remediate(self):
         return self.engine.remediate(self.resource, self.policy_id)
 
